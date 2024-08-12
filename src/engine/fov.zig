eaece7152c42a725ffc58b88e1e3e3e777c49bb4
isabel <ixthegoat@gmail.com>
2024-08-12 18:58:59 -0400

    cleanuo

--- src/engine/fov.zig
+++ src/engine/fov.zig
@@ -3,16 +3,17 @@ const entity = @import("entities.zig");
 
 const car_dist = @import("functions.zig").cartesian_distance;
 const euc_dist = @import("functions.zig").euc_dist;
+const sign_euc_dist = @import("functions.zig").sign_euc_dist;
+const plus_sign = @import("functions.zig").plus_sign;
 
-const radius: usize = 4;
+const radius: usize = 7;
 
 pub fn draw_fov(world_state: *world.World) void {
     const player: entity.Entity = world_state.entities[0];
 
     for (0..world.world_width - radius) |x| {
         for (0..world.world_height - radius) |y| {
-            if ((car_dist(player.xpos, player.ypos, x, y)) <= radius) {
-                //if ((euc_dist(player.xpos, x) <= radius) and (euc_dist(player.ypos, y) <= radius)) {
+            if (car_dist(player.xpos, player.ypos, x, y) <= radius) {
                 world_state.map[x][y].visible = true;
                 world_state.map[x][y].revealed = true;
             } else {
